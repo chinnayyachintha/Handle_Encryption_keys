0085023489e012d7d720e02756191bfd5647be0f
chinnayyachintha <chinthayadav6@gmail.com>
2024-11-12 17:24:13 +0530

    updated aduience

--- lambda_functions.tf
+++ lambda_functions.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "payment_processing_lambda" {
   environment {
     variables = {
       ISSUER                   = "payroc"                      # Unique identifier for the payment system
-      AUDIENCE                 = "payment-gateway.example.com" # Replace with actual DNS
+      AUDIENCE                 = "payment-processing-api" # Replace with actual DNS (or) API Gateway URL (or)  the name of the API
       JWT_SIGNING_KEY_ALIAS    = aws_kms_alias.jwt_signing_key_alias.arn
       JWT_ENCRYPTION_KEY_ALIAS = aws_kms_alias.encryption_key_alias.arn
     }
